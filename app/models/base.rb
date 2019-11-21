1fcfd555ebc062995fc36a208ab7fc71d8a78c50
EC2 Default User <ec2-user@ip-172-31-41-178.ap-northeast-1.compute.internal>
2019-11-21 11:37:36 +0000

    Baseのモデルとコントローラー作成

---
+++ app/models/base.rb
@@ -0,0 +1,7 @@
+class Base < ApplicationRecord
+    validates :base_name, presence: true, length: { maximum: 30 }
+    
+    validates :attendance_typ, presence: true, length: { maximum: 50 }
+    
+    validates :base_id, presence: true, length: { maximum: 8 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191117042131) do
+ActiveRecord::Schema.define(version: 20191121112708) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20191117042131) do
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
+  create_table "bases", force: :cascade do |t|
+    t.integer "base_id"
+    t.string "base_name"
+    t.string "attendance_typ"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -32,8 +40,8 @@ ActiveRecord::Schema.define(version: 20191117042131) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-11-18 23:00:00"
-    t.datetime "work_time", default: "2019-11-18 22:30:00"
+    t.datetime "basic_time", default: "2019-11-20 23:00:00"
+    t.datetime "work_time", default: "2019-11-20 22:30:00"
     t.integer "employee_number"
     t.integer "u_id"
     t.datetime "designated_work_start_time"
