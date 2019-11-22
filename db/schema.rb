906279bdca2c74acd9427b4d013dd4c9f7d1b7f9
EC2 Default User <ec2-user@ip-172-31-41-178.ap-northeast-1.compute.internal>
2019-11-22 13:57:45 +0000

    2019/11/22 u_id　カラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121112708) do
+ActiveRecord::Schema.define(version: 20191122135344) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 20191121112708) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2019-11-20 23:00:00"
-    t.datetime "work_time", default: "2019-11-20 22:30:00"
+    t.datetime "basic_time", default: "2019-11-21 23:00:00"
+    t.datetime "work_time", default: "2019-11-21 22:30:00"
     t.integer "employee_number"
-    t.integer "u_id"
+    t.string "u_id"
     t.datetime "designated_work_start_time"
     t.datetime "designated_work_end_time"
     t.boolean "superior", default: false
