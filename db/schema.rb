0d96543e6f68969def1bac1bd7bbc577100868d9
EC2 Default User <ec2-user@ip-172-31-37-135.ap-northeast-1.compute.internal>
2020-01-23 13:15:19 +0000

    １ヶ月分の勤怠申請機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -49,12 +49,12 @@ ActiveRecord::Schema.define(version: 20200122122251) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2020-01-21 23:00:00"
-    t.datetime "work_time", default: "2020-01-21 22:30:00"
+    t.datetime "basic_time", default: "2020-01-22 23:00:00"
+    t.datetime "work_time", default: "2020-01-22 22:30:00"
     t.integer "employee_number"
     t.string "u_id"
-    t.datetime "designated_work_start_time", default: "2020-01-21 23:00:00"
-    t.datetime "designated_work_end_time", default: "2020-01-22 09:00:00"
+    t.datetime "designated_work_start_time", default: "2020-01-22 23:00:00"
+    t.datetime "designated_work_end_time", default: "2020-01-23 09:00:00"
     t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
